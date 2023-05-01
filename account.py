34b56cdcf95e5d6308cb1dac76e1f2b3b39dadb1
Jacob Hortman <jacob.hortman@gmail.com>
2023-04-30 22:09:07 -0500

    Changed outline of project in main.py and started account.py.

---
+++ account.py
@@ -0,0 +1,16 @@
+class Account:
+    def __init__(self):
+        self.__balance = 0
+        self.__transactions = {}
+
+    def add_transaction(self):
+        # TODO: complete add_transaction method
+
+    def remove_transaction(self):
+        # TODO: complete remove_transaction method
+
+    def get_balance(self):
+        return self.__balance
+
+    def get_transactions(self):
+        return self.__transactions
--- main.py
+++ main.py
@@ -3,14 +3,13 @@ My Python application idea is a personal finance manager.
 
 The application will have the following features:
 
-    - Users can create an account and log in to track their expenses and income.
+    - Users can create an account to track their expenses and income.
     - Users can add categories for their expenses and income, such as "rent," "groceries," "salary,"
      "freelance income," etc.
     - Users can add transactions, including the date, category, amount, and a short description.
-    - Users can view their expenses and income over time, filtered by date range and category.
     - Users can view summary statistics, such as total income, total expenses, net income,
      and average spending per category.
-    - Users can export their data to a CSV file or a database may be created to store and process user data (undecided).
+    - Users can export their data to a CSV file.
 
 To implement this application, the following components will be used:
 
@@ -22,25 +21,19 @@ To implement this application, the following components will be used:
     	- Handle runtime errors, such as database connection errors, file I/O errors, and user input errors.
     	- Use try-except blocks to handle exceptions gracefully.
 
-    4.) An intuitive GUI for the application:
-    	- Use a GUI framework, such as PyQt or Tkinter, to create an interactive interface.
-    	- Design an easy-to-use interface for adding and viewing transactions, filtering data, and exporting data.
-
-    5.) Code organized with the help of classes or modules:
+    4.) Code organized with the help of classes or modules:
     	- Use classes to encapsulate data and behavior.
     	- Use modules to separate concerns and improve code readability and maintainability.
 
-    6.) Data stored in files/databases:
-    	- Use a database, such as SQLite or MySQL, to store user data.
+    5.) Data stored in files/databases:
     	- Use a CSV file to export user data.
 
-    7.) Proper code documentation:
+    6.) Proper code documentation:
     	- Use descriptive variable names to make code more readable.
     	- Use docstrings for functions/methods to document their purpose and input/output parameters.
     	- Use type hinting to make code more maintainable and easier to understand.
     	- Use descriptive commit messages to track changes and document the development process.
 
 Overall, this personal finance manager is a complex Python application that incorporates various elements of software
-development, including validation, exception handling, GUI development, code organization, data storage,
-and documentation.
+development, including validation, exception handling, code organization, data storage, and documentation.
 """
