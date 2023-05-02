95957685dc009a3ba7d43068172347bb9f337233
Jacob Hortman <jacob.hortman@gmail.com>
2023-05-02 14:24:05 -0500

    Worked on logic in main.py, and fixed methods in account.py.

--- account.py
+++ account.py
@@ -1,21 +1,56 @@
+from typing import Dict, Any
+
+
 class Account:
+    __transaction_number: int
+    __transactions: dict[Any, Any]
+    __balance: float
+
     def __init__(self):
+        """
+        Method to initialize an Account object.
+        """
         self.__balance = 0
         self.__transactions = {}
         self.__transaction_number = 1
 
-    def add_transaction(self, category, date, amount, description):
+    def add_transaction(self, category: str = '', date: str = '', amount: float = 0, description: str = '') -> None:
+        """
+        Method to add a transaction to the Account object.
+        :param category: Category for the transaction.
+        :param date: Date for the transaction.
+        :param amount: Dollar amount for the transaction.
+        :param description: Description for the transaction.
+        """
         self.__transactions[self.__transaction_number] = [category, date, amount, description]
         self.__transaction_number += 1
+        self.__balance += amount
 
-    def remove_transaction(self, transaction):
+    def remove_transaction(self, transaction: int) -> None:
+        """
+        Method to remove a transaction from the Account object.
+        :param transaction: Number of the transaction.
+        """
+        self.__balance -= self.__transactions[transaction][2]
         self.__transactions.pop(transaction)
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+        """
+         Method to return the current balance of the Account object.
+        :return: Returns the current balance as a float
+        """
         return self.__balance
 
-    def get_transactions(self):
+    def get_transactions(self) -> dict:
+        """
+        Method to return the transactions of the Account object.
+        :return: Returns the dictionary of transactions.
+        """
         return self.__transactions
 
-    def get_transaction_number(self):
+    def get_transaction_number(self) -> int:
+        """
+        Method to return the current transaction number of the Account object.
+        :return: Returns the current transaction number as an int
+        """
         return self.__transaction_number
--- main.py
+++ main.py
@@ -37,47 +37,118 @@ To implement this application, the following components will be used:
 Overall, this personal finance manager is a complex Python application that incorporates various elements of software
 development, including validation, exception handling, code organization, data storage, and documentation.
 """
-from account import *
 import os
+from account import Account
+import csv
 
 
-def create_account():
-    user_account = Account()
-    account_choice = ''
+def create_account(user_account: Account = Account()) -> None:
+    """
+    A function to create a user account and allow user to select choices to interact with their account.
+    """
+    print('Welcome to your account!')
+    account_choice: str = ''
     while account_choice != 'q':
-        print('Welcome to your account!')
         print('Please choose an option:')
         account_choice = input('q - quit\nn - new transaction\nr - remove a transaction\ne - export account info to '
-                               'csv file\nv - view all transactions\nb - view current balance')
+                               'csv file\nv - view all transactions\nb - view current balance\n')
         if account_choice == 'n':
-            category = input(f'Enter a category for transaction number:{user_account.get_transaction_number()}\n')
-            date = input('Enter a date for the transaction:\n')
-            amount = float(input('Enter an amount for the transaction:\n'))
-            description = input('Enter a short description for the transaction:\n')
-            user_account.add_transaction(category, date, amount, description)
+            try:
+                category: str = input(f'Enter a category for transaction number '
+                                      f'{user_account.get_transaction_number()}\n')
+                date: str = input('Enter a date for the transaction:\n')
+                amount: float = float(input('Enter an amount for the transaction:\n'))
+                description: str = input('Enter a short description for the transaction:\n')
+                user_account.add_transaction(category, date, amount, description)
+                print(f'The following transaction was added:\n'
+                      f'Category: {user_account.get_transactions()[user_account.get_transaction_number() - 1][0]}\n'
+                      f'Date: {user_account.get_transactions()[user_account.get_transaction_number() - 1][1]}\n'
+                      f'Amount: {user_account.get_transactions()[user_account.get_transaction_number() - 1][2]:.2f}\n'
+                      f'Description: {user_account.get_transactions()[user_account.get_transaction_number() - 1][3]}\n')
+            except ValueError:
+                print('Must enter a number for transaction amount!\n')
         elif account_choice == 'r':
-            transaction_to_remove = input('Choose a transaction number to remove:\n')
-            user_account.remove_transaction(transaction_to_remove)
+            try:
+                transaction_to_remove: int = int(input('Choose a transaction number to remove:\n'))
+                user_account.remove_transaction(transaction_to_remove)
+            except ValueError:
+                print('Must enter a number!\n')
+            except KeyError:
+                print('Invalid transaction number!\n')
         elif account_choice == 'e':
-            pass
+            output_file = input('Enter a name for the output file:\n').strip()
+            while os.path.isfile('files/' + output_file):
+                response = input('Overwrite existing file (y/n): ').strip().lower()
+
+                while response != 'y' and response != 'n':
+                    response = input('Enter (y/n): ').strip().lower()
+
+                if response == 'y':
+                    break
+                elif response == 'n':
+                    filename = input('New output file name: ').strip()
+            with open(('files/' + output_file), 'w', newline='') as csv_output:
+                csv_content = csv.writer(csv_output)
+                csv_content.writerow(['Category', 'Date', 'Amount', 'Description'])
+                account_transactions = user_account.get_transactions()
+                for transaction in account_transactions:
+                    csv_content.writerow([account_transactions[transaction][0], account_transactions[transaction][1],
+                                          account_transactions[transaction][2], account_transactions[transaction][3]])
+            print('Account saved!\n')
         elif account_choice == 'v':
-            print(user_account.get_transactions())
+            account_transactions = user_account.get_transactions()
+            for transaction in account_transactions:
+                print(f'Transaction {transaction}:\n'
+                      f'Category: {account_transactions[transaction][0]}\n'
+                      f'Date: {account_transactions[transaction][1]}\n'
+                      f'Amount: {account_transactions[transaction][2]:.2f}\n'
+                      f'Description: {account_transactions[transaction][3]}\n')
         elif account_choice == 'b':
-            print(f'Current balance is: {user_account.get_balance()}')
+            print(f'Current balance is: ${user_account.get_balance():.2f}\n')
+        elif account_choice == 'q':
+            pass
+        else:
+            print('Must enter a valid option!\n')
 
 
-def load_account_from_csv(file_path):
-    pass
+def load_account_from_csv(file_path: str) -> None:
+    """
+    A function to allow loading of an account from a csv file.
+    :param file_path: Variable containing the path of the csv file.
+    """
+    user_account = Account()
+    try:
+        with open(file_path, 'r') as input_file:
+            csv_reader = csv.reader(input_file)
+            for line in csv_reader:
+                print(line)
+                category = line[0]
+                date = line[1]
+                amount = float(line[2])
+                description = line[3]
+                user_account.add_transaction(category, date, amount, description)
+
+        create_account(user_account)
+    except StopIteration:
+        print('Invalid csv file structure!\n')
 
 
 if __name__ == '__main__':
     print('Welcome to the personal finance manager!')
-    user_choice = ''
+    user_choice: str = ''
     while user_choice != 'q':
         print('Please choose an option:')
         user_choice = input('q - quit\nn - new account\nr - read account info from csv file\n')
         if user_choice == 'n':
             create_account()
         elif user_choice == 'r':
-            csv_path = input('Please input the path for the csv file.\n')
-            load_account_from_csv(csv_path)
+            csv_name: str = input('Please input the name of the csv file.\n').strip()
+            if os.path.isfile('files/' + csv_name):
+                file_path = 'files/' + csv_name
+                load_account_from_csv(file_path)
+            else:
+                print('File not found!\n')
+        elif user_choice == 'q':
+            pass
+        else:
+            print('Must enter a valid option!\n')
